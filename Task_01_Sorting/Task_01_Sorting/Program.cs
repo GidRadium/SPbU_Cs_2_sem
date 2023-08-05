da22352d2ecee035b5efce459c8004022304b586
GitRadium <gidradiumcode@gmail.com>
2023-08-05 13:55:13 +0300

    Create Task_01.Sorting.Main()

--- Task_01_Sorting/Task_01_Sorting/Program.cs
+++ Task_01_Sorting/Task_01_Sorting/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Task_01
+{
+    class Sorting
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter numbers separated by spaces:");
+            string input = Console.ReadLine();
+            int[] array =  input.Split(' ').Select(int.Parse).ToArray();
+
+            BubbleSort(array);
+
+            foreach (var item in array)
+                Console.Write($"{item} ");
+
+        }
+
+        static int[] BubbleSort(int[] array)
+        {
+            return array;
+        }
+    }
+}

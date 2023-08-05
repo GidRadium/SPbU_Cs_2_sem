93f5c4ca6dea87e38ec43e556159d2131dcff00a
GitRadium <gidradiumcode@gmail.com>
2023-08-05 12:55:27 +0300

    Create nUnit sample

---
+++ Task_01_Sorting/Task_01_Sorting/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Task_01_Sorting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

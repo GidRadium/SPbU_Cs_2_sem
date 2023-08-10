bef80dad31a784619ebcc6d5b368e45b7c9fac78
GitRadium <gidradiumcode@gmail.com>
2023-08-10 19:37:53 +0300

    Add BWT definitions

---
+++ Task_02_BWT/Task_02_BWT/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace Task_02_BWT;
+
+class BWT
+{
+    static void Main()
+    {
+
+    }
+
+    public static Tuple<string, int> Encode(string input)
+    {
+
+    }
+
+    public static string Decode(string encoded, int index)
+    {
+
+    }
+}

94bbb492828831d9a4ce636c60e654c5e27bbf93
GitRadium <gidradiumcode@gmail.com>
2023-09-30 13:06:19 +0300

    First Commit with part of Trie

---
+++ Task_03_Trie/Task_03_Trie/Trie.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_03_Trie
+{
+    internal class Trie
+    {
+        private class Node
+        {
+            public Dictionary<char, Node> Children;
+            public int WordsPassedNumber;
+            public bool IsEndOfWord;
+
+            public Node()
+            {
+                this.Children = new Dictionary<char, Node>();
+                this.WordsPassedNumber = 0;
+                this.IsEndOfWord = false;
+            }
+        }
+
+        private Node Root;
+
+        public int Size { get; private set; }
+
+        public Trie()
+        {
+            this.Root = new Node();
+        }
+
+        private bool AddToNode(Node node, string elementSuffix)
+        {
+
+        }
+
+        bool Add(string element)
+        {
+
+        }
+
+        bool Contains(string element) { }
+
+        bool Remove(string element) { }
+
+        int HowManyStartsWithPrefix(string prefix) { }
+    }
+}

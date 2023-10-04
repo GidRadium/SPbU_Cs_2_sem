c5f0ef0dde369bcfb72920d517522c010e3513bc
GitRadium <gidradiumcode@gmail.com>
2023-10-04 10:03:46 +0300

    Create Main()

--- Task_05_LZW/Task_05_LZW/Program.cs
+++ Task_05_LZW/Task_05_LZW/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Task_05_LWZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Invalid number of command line arguments.");
+                return;
+            }
+
+            string filePath = Path.GetFullPath(args[0]);
+            if (!File.Exists(filePath)) {
+                Console.WriteLine($"File {filePath} does not exist.");
+                return;
+            }
+
+            string option = args[1];
+
+            switch (option)
+            {
+                case "-c":
+                    //Compress(filePath);
+                    break;
+                case "-u":
+                    //Decompress(filePath);
+                    break;
+                default:
+                    Console.WriteLine($"Invalid option: {option}. Available options: -c, -u");
+                    return;
+            }
+        }
+    }
+}

0f7845027d8831d008b105baf14eea5a4bcecb23
GitRadium <gidradiumcode@gmail.com>
2023-10-05 20:12:04 +0300

    Add AddValue()

---
+++ Task_07_UniqueList/Task_07_UniqueList/MyList.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_07_UniqueList;
+internal class MyList
+{
+    public int Size { get; private set; }
+
+    protected class Node
+    {
+        public int Value;
+        public Node? Next;
+    }
+
+    private Node? Root = null;
+
+    public virtual void AddValue(int value, int index)
+    {
+        if (index < 0 || index > this.Size)
+            throw new ArgumentOutOfRangeException();
+
+        var node = new Node();
+        node.Value = value;
+        
+        if (this.Size == 0)
+        {
+            this.Root = node;
+            this.Size++;
+            return;
+        }
+        
+        if (index == 0)
+        {
+            node.Next = this.Root;
+            this.Root = node;
+            this.Size++;
+            return;
+        }
+
+        var temp = this.Root;
+        for (int i = 0; i < this.Size; i++)
+            temp = temp.Next;
+
+        node.Next = temp.Next;
+        temp.Next = node;
+        Size++;
+    }
+
+    public int GetValue(int index)
+    {
+        return 0;
+    }
+
+    public virtual void SetValue(int value, int index)
+    {
+
+    }
+
+    public virtual void DeleteValue(int index)
+    {
+
+    }
+
+    public bool Contains(int value)
+    {
+        return false;
+    }
+}
---
+++ Task_07_UniqueList/Task_07_UniqueList/UniqueList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_07_UniqueList
+{
+    internal class UniqueList : MyList
+    {
+    }
+}

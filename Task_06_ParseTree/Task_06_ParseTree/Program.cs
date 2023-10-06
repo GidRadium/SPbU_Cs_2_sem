203335d3d296ec3d9d0bfde0892baf9729bf90dd
GitRadium <gidradiumcode@gmail.com>
2023-10-06 17:08:30 +0300

    It is really working

--- Task_06_ParseTree/Task_06_ParseTree/Program.cs
+++ Task_06_ParseTree/Task_06_ParseTree/Program.cs
@@ -4,7 +4,52 @@ public class Program
 {
     static void Main()
     {
-        var tree = new ParseTree("(* (+ 1 1) 2)");
+        Console.WriteLine("Enter file with expression path:");
+        string filePath = Console.ReadLine() ?? string.Empty;
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine($"Can't open file {filePath}");
+            return;
+        }
+
+        string input;
+        using (var file = new System.IO.StreamReader(filePath))
+        {
+            input = file.ReadToEnd();
+        }
+
+        input = input.Trim();
+        ParseTree tree;
+        try
+        {
+            tree = new ParseTree(input);
+        }
+        catch (IncorrectExpressionException)
+        {
+            Console.WriteLine($"Expression in file {filePath} is incorrect!");
+            return;
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Undefined exception is catched:\n{ex.Message}");
+            return;
+        }
+        
         Console.WriteLine(tree);
+
+        try
+        {
+            Console.WriteLine($"Result of solving expression: {tree.Calculate()}");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Expression contains dividing by 0");
+            return;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Undefined exception is catched:\n{ex.Message}");
+            return;
+        }
     }
 }

0d2588f1aa730f9db57838411cce0478efb14cad
GitRadium <gidradiumcode@gmail.com>
2023-10-22 19:00:32 +0300

    Correct the solution

--- Task03Trie/Task03Trie/Trie.cs
+++ Task03Trie/Task03Trie/Trie.cs
@@ -1,131 +1,121 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
+﻿namespace Task03Trie;
 
-namespace Task_03_Trie
+public class Trie
 {
-    public class Trie
+    private class Node
     {
-        private class Node
-        {
-            public Dictionary<char, Node> Children;
-            public int WordsPassedNumber;
-            public bool IsEndOfWord;
+        internal Dictionary<char, Node> Children { get; set; }
+        internal int WordsPassedNumber { get; set; }
+        internal bool IsEndOfWord { get; set; }
 
-            public Node()
-            {
-                this.Children = new Dictionary<char, Node>();
-                this.WordsPassedNumber = 0;
-                this.IsEndOfWord = false;
-            }
+        public Node()
+        {
+            this.Children = new Dictionary<char, Node>();
+            this.WordsPassedNumber = 0;
+            this.IsEndOfWord = false;
         }
+    }
 
-        private Node Root;
-        public int Size { get; private set; }
-        private bool ContainsEmptyString;
+    private Node root;
+    public int Size { get; private set; }
+    private bool containsEmptyString;
 
-        public Trie()
+    public Trie()
+    {
+        this.root = new Node();
+    }
+
+    public bool Add(string element)
+    {
+        if (element == null || Contains(element))
+            return false;
+
+        if (element.Length == 0)
         {
-            this.Root = new Node();
-            this.Size = 0;
-            this.ContainsEmptyString = false;
-        }
-
-        public bool Add(string element)
-        {
-            if (element == null || Contains(element))
-                return false;
-
-            if (element.Length == 0)
-            {
-                this.ContainsEmptyString = true;
-                this.Size++;
-                return true;
-            }
-
-            Node temp = this.Root;
-            for (int i = 0; i < element.Length; i++)
-            {
-                if (!temp.Children.ContainsKey(element[i]))
-                    temp.Children.Add(element[i], new Node());
-                temp = temp.Children[element[i]];
-                temp.WordsPassedNumber++;
-            }
-
-            temp.IsEndOfWord = true;
+            this.containsEmptyString = true;
             this.Size++;
-
             return true;
         }
 
-        public bool Contains(string element)
+        Node temp = this.root;
+        for (int i = 0; i < element.Length; i++)
         {
-            if (element == null) 
-                return false;
-            if (element.Length == 0)
-                return this.ContainsEmptyString;
-
-            Node temp = this.Root;
-            for (int i = 0; i < element.Length; i++)
-            {
-                if (!temp.Children.ContainsKey(element[i]))
-                    return false;
-                temp = temp.Children[element[i]];
-            }
-
-            return temp.IsEndOfWord;
+            if (!temp.Children.ContainsKey(element[i]))
+                temp.Children.Add(element[i], new Node());
+            temp = temp.Children[element[i]];
+            temp.WordsPassedNumber++;
         }
 
-        public bool Remove(string element)
-        {
-            if (element == null || !Contains(element))
-                return false;
+        temp.IsEndOfWord = true;
+        this.Size++;
 
-            if (element.Length == 0)
+        return true;
+    }
+
+    public bool Contains(string element)
+    {
+        if (element == null) 
+            return false;
+        if (element.Length == 0)
+            return this.containsEmptyString;
+
+        Node temp = this.root;
+        for (int i = 0; i < element.Length; i++)
+        {
+            if (!temp.Children.ContainsKey(element[i]))
+                return false;
+            temp = temp.Children[element[i]];
+        }
+
+        return temp.IsEndOfWord;
+    }
+
+    public bool Remove(string element)
+    {
+        if (element == null || !Contains(element))
+            return false;
+
+        if (element.Length == 0)
+        {
+            this.containsEmptyString = false;
+            this.Size--;
+            return true;
+        }
+
+        this.Size--;
+        Node temp = this.root;
+        for (int i = 0; i < element.Length; i++)
+        {
+            if (temp.Children[element[i]].WordsPassedNumber == 1)
             {
-                this.ContainsEmptyString = false;
-                this.Size--;
+                temp.Children.Remove(element[i]);
                 return true;
             }
 
-            this.Size--;
-            Node temp = this.Root;
-            for (int i = 0; i < element.Length; i++)
-            {
-                if (temp.Children[element[i]].WordsPassedNumber == 1)
-                {
-                    temp.Children.Remove(element[i]);
-                    return true;
-                }
-
-                temp = temp.Children[element[i]];
-                temp.WordsPassedNumber--;
-            }
-
-            temp.IsEndOfWord = false;
-
-            return true;
+            temp = temp.Children[element[i]];
+            temp.WordsPassedNumber--;
         }
 
-        public int HowManyStartsWithPrefix(string prefix)
+        temp.IsEndOfWord = false;
+
+        return true;
+    }
+
+    public int HowManyStartsWithPrefix(string prefix)
+    {
+        if (prefix == null)
+            return 0;
+        if (prefix.Length == 0)
+            return this.containsEmptyString ? 1 : 0;
+
+        Node temp = this.root;
+        for (int i = 0; i < prefix.Length; i++)
         {
-            if (prefix == null)
+            if (!temp.Children.ContainsKey(prefix[i]))
                 return 0;
-            if (prefix.Length == 0)
-                return this.ContainsEmptyString ? 1 : 0;
-
-            Node temp = this.Root;
-            for (int i = 0; i < prefix.Length; i++)
-            {
-                if (!temp.Children.ContainsKey(prefix[i]))
-                    return 0;
-                temp = temp.Children[prefix[i]];
-            }
-
-            return temp.WordsPassedNumber;
+            temp = temp.Children[prefix[i]];
         }
+
+        return temp.WordsPassedNumber;
     }
 }

c1c1b0acb7287642831291aef48acd5d59411788
GitRadium <gidradiumcode@gmail.com>
2023-10-23 10:55:16 +0300

    Correct the solution

--- Task07UniqueList/Task07UniqueList/MyList.cs
+++ Task07UniqueList/Task07UniqueList/MyList.cs
@@ -1,21 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Task_07_UniqueList;
+﻿namespace Task07UniqueList;
 public class MyList
 {
     public int Size { get; private set; }
 
     protected class Node
     {
-        public int Value;
-        public Node? Next;
+        internal int Value { get; set; }
+        internal Node? Next { get; set; }
     }
 
-    private Node? Root = null;
+    private Node? root = null;
 
     public virtual void AddValue(int value, int index)
     {
@@ -27,20 +21,20 @@ public class MyList
         
         if (this.Size == 0)
         {
-            this.Root = node;
+            this.root = node;
             this.Size++;
             return;
         }
         
         if (index == 0)
         {
-            node.Next = this.Root;
-            this.Root = node;
+            node.Next = this.root;
+            this.root = node;
             this.Size++;
             return;
         }
 
-        var temp = this.Root;
+        var temp = this.root;
         for (int i = 0; i < index - 1; i++)
             temp = temp.Next;
 
@@ -54,7 +48,7 @@ public class MyList
         if (index < 0 || index > this.Size - 1)
             throw new ArgumentOutOfRangeException();
 
-        var temp = this.Root;
+        var temp = this.root;
         for (int i = 0; i < index; i++)
             temp = temp.Next;
 
@@ -66,7 +60,7 @@ public class MyList
         if (index < 0 || index > this.Size - 1)
             throw new ArgumentOutOfRangeException();
 
-        var temp = this.Root;
+        var temp = this.root;
         for (int i = 0; i < index; i++)
             temp = temp.Next;
 
@@ -80,12 +74,12 @@ public class MyList
 
         if (index == 0)
         {
-            this.Root = this.Root.Next;
+            this.root = this.root.Next;
             this.Size--;
             return;
         }
 
-        var temp = this.Root;
+        var temp = this.root;
         for (int i = 0; i < index; i++)
             temp = temp.Next;
 
@@ -94,13 +88,11 @@ public class MyList
     }
 
     public bool Contains(int value)
-    {
-        return FindIndexByValue(value) >= 0;
-    }
+        => FindIndexByValue(value) >= 0;
 
     public int FindIndexByValue(int value)
     {
-        var temp = this.Root;
+        var temp = this.root;
         for (int i = 0; i < this.Size; i++)
         {
             if (temp.Value == value)
--- Task07UniqueList/Task07UniqueList/UniqueList.cs
+++ Task07UniqueList/Task07UniqueList/UniqueList.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Task_07_UniqueList;
+﻿namespace Task07UniqueList;
 
 public class AddRepeatingElementToUniqueListException : ArgumentException { };
 

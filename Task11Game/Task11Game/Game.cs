bfa081b183db4f1e1de0687105ec5646e1546ece
GitRadium <gidradiumcode@gmail.com>
2023-10-23 14:29:37 +0300

    Testing example from lesson

---
+++ Task11Game/Task11Game/Game.cs
@@ -0,0 +1,16 @@
+﻿namespace Task11Game;
+
+public class Game
+{
+    public void OnLeft(object sender, EventArgs args)
+        => Console.WriteLine("Going left");
+
+    public void OnRight(object sender, EventArgs args)
+        => Console.WriteLine("Going right");
+
+    public void OnUp(object sender, EventArgs args)
+        => Console.WriteLine("Going up");
+
+    public void OnDown(object sender, EventArgs args)
+        => Console.WriteLine("Going down");
+}
--- Task11Game/Task11Game/Program.cs
+++ Task11Game/Task11Game/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Task11Game;
+
+var eventLoop = new EventLoop();
+var game = new Game();
+
+eventLoop.LeftHandler += game.OnLeft;
+eventLoop.RightHandler += game.OnRight;
+eventLoop.UpHandler += game.OnUp;
+eventLoop.DownHandler += game.OnDown;
+
+var log = new List<string>();
+eventLoop.LeftHandler += (sender, eventArgs) => log.Add("left");
+eventLoop.RightHandler += (sender, eventArgs) => log.Add("right");
+eventLoop.Run();

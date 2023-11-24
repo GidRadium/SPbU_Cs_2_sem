3387226184f79a6368848ce3ddbddf36855d7c5d
GitRadium <gidradiumcode@gmail.com>
2023-11-24 12:32:48 +0300

    Correct the solution

--- Task10MapFilterFold/Task10MapFilterFold/MyFunctions.cs
+++ Task10MapFilterFold/Task10MapFilterFold/MyFunctions.cs
@@ -1,7 +1,9 @@
 ﻿namespace Task10MapFilterFold;
 
+// Some useful functions for data processing.
 static public class MyFunctions
 {
+    // Returns a list obtained by applying the passed function to each element of the passed list.
     public static List<TOut> Map<TIn, TOut>(List<TIn> list, Func<TIn, TOut> mapFunction)
     {
         var output = new List<TOut>();
@@ -11,6 +13,7 @@ static public class MyFunctions
         return output;
     }
 
+    // Returns a list made up of those elements of the passed list for which the passed function returned true.
     public static List<TIn> Filter<TIn>(List<TIn> list, Func<TIn, bool> filterFunction)
     {
         var output = new List<TIn>();
@@ -21,6 +24,7 @@ static public class MyFunctions
         return output;
     }
 
+    // Returns the accumulated value obtained after the entire list has been passed through.
     public static TOut Fold<TIn, TOut>(List<TIn> list, TOut startValue, Func<TIn, TOut, TOut> foldFunction)
     {
         var output = startValue;
